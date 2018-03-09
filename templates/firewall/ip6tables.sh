3688a36c3ab0beff90432e2124d382aea740928d
DO1JLR <l3d@see-base.de>
2018-03-09 16:45:29 +0100

    Fix firewall for gw04

--- templates/firewall/ip6tables.sh
+++ templates/firewall/ip6tables.sh
@@ -4,9 +4,12 @@
 #
 # Auch beim IPv6 gibt es bischen zu tun
 #
+{% if wan_ipv6_ip: != "" %}
+
 /sbin/ip6tables -A INPUT -i bat0 -p ipv6-icmp -m icmp6 --icmpv6-type 134 -j DROP
 /sbin/ip6tables -A FORWARD -m state --state RELATED,ESTABLISHED -j ACCEPT
 /sbin/ip6tables -A FORWARD -m state --state NEW -m connlimit --connlimit-above 200 --connlimit-mask 128 --connlimit-saddr -j DROP
 /sbin/ip6tables -A FORWARD -i bat0 -o {{ wan_interface }} -j ACCEPT
 /sbin/ip6tables -t nat -A POSTROUTING -o {{ wan_interface }} -j NETMAP --to {{ wan_ipv6_network }}
 
+{% endif %}

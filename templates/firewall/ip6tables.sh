ffd38be568613b9bdc7fb1aa02ca56d81ebb9f81
L3D <l3d@see-base.de>
2019-02-08 23:32:19 +0100

    improve ipv6 firewall for hetzner

--- templates/firewall/ip6tables.sh
+++ templates/firewall/ip6tables.sh
@@ -10,8 +10,18 @@
 /sbin/ip6tables -A FORWARD -m state --state RELATED,ESTABLISHED -j ACCEPT
 /sbin/ip6tables -A FORWARD -m state --state NEW -m connlimit --connlimit-above 200 --connlimit-mask 128 --connlimit-saddr -j DROP
 /sbin/ip6tables -A FORWARD -i bat0 -o {{ wan_interface }} -j ACCEPT
+
+# Block ULAs to global network
+/sbin/ip6tables -A FORWARD -d fc00::/7 ! -o {{ wan_interface }} -j ACCEPT
+/sbin/ip6tables -A FORWARD -d fec0::/10 ! -o {{ wan_interface }} -j ACCEPT
+/sbin/ip6tables -A FORWARD -j DROP 
+
+#
+# NAT Table
+#
 /sbin/ip6tables -t nat -A POSTROUTING -o {{ wan_interface }} -j NETMAP --to {{ wan_ipv6_network }}
 {% if vpn_on_port_443 == 'true' %}#
+#
 # ICVPN Firewall Optionen
 #
 /sbin/iptables -A FORWARD -i {{ ipv4_dhcp_interface }} -o {{ icvpn_interface }} -j ACCEPT

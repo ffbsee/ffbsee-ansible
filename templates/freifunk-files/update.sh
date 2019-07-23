238f50fb668dab6422ec9c67d684f53c9f0d8893
Lilian Roller <lilian.roller@wingcon.com>
2019-07-23 14:47:54 +0200

    change speed to 100m

--- templates/freifunk-files/update.sh
+++ templates/freifunk-files/update.sh
@@ -237,7 +237,7 @@ if [ $run_gateway = true ]; then
                 fi
         fi
         # Activate the gateway announcements on a node that has a DHCP server running
-        batctl gw_mode server 10mbit/10mbit
+        batctl gw_mode server 100mbit/100mbit
 fi # run_gateway
 if [ $run_map = true ]; then
         #collect all map pieces
